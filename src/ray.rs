f4fc0a5609c00aefb6b812395146b14e6817becb
Trevor Elliott <awesomelyawesome@gmail.com>
2019-07-21 18:38:53 -0700

    Boost the number of default steps

--- src/ray.rs
+++ src/ray.rs
@@ -9,7 +9,7 @@ pub struct RayConfig {
 }
 
 impl RayConfig {
-    pub const MAX_STEPS: usize = 100;
+    pub const MAX_STEPS: usize = 1000;
     pub const MIN_DIST: f32 = 0.001;
     pub const MAX_DIST: f32 = 100.0;
 }
