076ea5fb7cbeedd7c2c53d2cec32aec6ea6873b4
Trevor Elliott <awesomelyawesome@gmail.com>
2019-09-01 11:07:54 -0700

    More compact BVH

--- src/bounding_volume.rs
+++ src/bounding_volume.rs
@@ -117,98 +117,102 @@ impl AABB {
 #[derive(Debug)]
 pub enum Axis { X, Y, Z }
 
-#[derive(Debug,Clone)]
-pub struct BVHNodeId(usize);
-
 #[derive(Debug)]
-pub enum BVHNode<T> {
+pub enum BVHNodeType {
     Internal {
-        left: Option<BVHNodeId>,
-        right: Option<BVHNodeId>,
-        bounds: AABB,
+        right_offset: usize,
         axis: Axis,
     },
     Leaf {
-        bounds: AABB,
-        value: T,
+        values_start: usize,
+        num_nodes: usize,
     },
 }
 
-impl<T> BVHNode<T> {
+#[derive(Debug)]
+pub struct BVHNode {
+    node_type: BVHNodeType,
+    bounds: AABB,
+}
+
+impl BVHNode {
     pub fn internal(bounds: AABB, axis: Axis) -> Self {
-        BVHNode::Internal{ left: None, right: None, bounds, axis }
+        BVHNode{
+            node_type: BVHNodeType::Internal{ right_offset: 0, axis },
+            bounds,
+        }
     }
 
-    pub fn leaf(bounds: AABB, value: T) -> Self {
-        BVHNode::Leaf{ bounds, value }
-    }
-
-    pub fn bounds<'a>(&'a self) -> &'a AABB {
-        match self {
-            BVHNode::Internal{ ref bounds, .. } => bounds,
-            BVHNode::Leaf{ ref bounds, .. } => bounds,
+    pub fn leaf(bounds: AABB, values_start: usize, num_nodes: usize) -> Self {
+        BVHNode{
+            node_type: BVHNodeType::Leaf{
+                values_start,
+                num_nodes,
+            },
+            bounds,
         }
     }
 }
 
 #[derive(Debug)]
 pub struct BVH<T> {
-    nodes: Vec<BVHNode<T>>,
+    nodes: Vec<BVHNode>,
+    values: Vec<T>,
 }
 
 impl<T> BVH<T> where T: Clone {
 
-    pub fn new() -> Self {
-        BVH{ nodes: Vec::new() }
-    }
-
-    pub fn from_nodes<GetBound>(nodes: &mut [T], get_bound: &GetBound) -> Self
+    pub fn from_nodes<GetBound>(mut values: Vec<T>, get_bound: &GetBound) -> Self
         where GetBound: Fn(&T) -> &AABB
     {
-        let mut bvh = Self::new();
-
-        if nodes.is_empty() {
-            return bvh;
+        if values.is_empty() {
+            BVH { nodes: Vec::new(), values }
+        } else {
+            let mut nodes = Vec::new();
+            Self::add_nodes(&mut nodes, &mut values, 0, get_bound);
+            BVH { nodes, values }
         }
-
-        bvh.add_nodes(nodes, get_bound);
-        bvh
     }
 
-    fn add_node(&mut self, node: BVHNode<T>) -> BVHNodeId {
-        self.nodes.push(node);
-        BVHNodeId(self.nodes.len() - 1)
+    fn add_node(nodes: &mut Vec<BVHNode>, node: BVHNode) -> usize {
+        nodes.push(node);
+        nodes.len() - 1
     }
 
-    fn get_node_mut<'a>(&'a mut self, nid: BVHNodeId) -> &'a mut BVHNode<T> {
-        unsafe { self.nodes.get_unchecked_mut(nid.0) }
+    fn get_node_mut<'a>(nodes: &'a mut Vec<BVHNode>, nid: usize) -> &'a mut BVHNode {
+        unsafe { nodes.get_unchecked_mut(nid) }
     }
 
-    fn add_nodes<GetBound>(&mut self, nodes: &mut [T], get_bound: &GetBound) -> Option<BVHNodeId>
+    fn add_nodes<GetBound>(
+        nodes: &mut Vec<BVHNode>,
+        values: &mut [T],
+        start: usize,
+        get_bound: &GetBound,
+    ) -> usize
         where GetBound: Fn(&T) -> &AABB
     {
-        if nodes.is_empty() {
-            return None
-        }
+        let mut bounds = get_bound(&values[0]).clone();
 
-        let mut bounds = get_bound(&nodes[0]).clone();
-
-        if nodes.len() == 1 {
-            return Some(self.add_node(BVHNode::leaf(bounds, nodes[0].clone())));
+        if values.len() == 1 {
         }
 
         let mut centroid_bound = AABB::from_point(bounds.centroid());
 
-        for node in &nodes[1..] {
-            let bound = get_bound(node);
+        for value in &values[1..] {
+            let bound = get_bound(value);
             bounds.union_mut(bound);
             centroid_bound.union_point_mut(&bound.centroid());
         }
 
-        // partition the nodes about the mid-point of the centroid bound
+        // if the centroid bound collapses to a single point, make a leaf
+        if centroid_bound.min == centroid_bound.max {
+            return Self::add_node(nodes, BVHNode::leaf(bounds, start, values.len()));
+        }
+
+        // partition the values about the mid-point of the centroid bound
         let (axis,mid) = centroid_bound.max_axis();
-        let pivot_index = utils::partition_by(nodes, |node| {
-            let bound = get_bound(&node);
+        let pivot_index = utils::partition_by(values, |value| {
+            let bound = get_bound(&value);
             match axis {
                 Axis::X => bound.centroid().x < mid,
                 Axis::Y => bound.centroid().y < mid,
@@ -216,50 +220,50 @@ impl<T> BVH<T> where T: Clone {
             }
         }).expect("Invalid centroid bound");
 
-        let nid = self.add_node(BVHNode::internal(bounds, axis));
+        let nid = Self::add_node(nodes, BVHNode::internal(bounds, axis));
 
-        let lid = self.add_nodes(&mut nodes[0..pivot_index], get_bound);
-        let rid = self.add_nodes(&mut nodes[pivot_index..], get_bound);
+        let _loff = Self::add_nodes(nodes, &mut values[0..pivot_index], start, get_bound);
+        let roff = Self::add_nodes(nodes, &mut values[pivot_index..], start + pivot_index, get_bound);
 
-        if let BVHNode::Internal{ ref mut left, ref mut right, .. } =
-            self.get_node_mut(nid.clone()) {
-            *left = lid;
-            *right = rid;
+        let BVHNode{ node_type, .. } = Self::get_node_mut(nodes, nid);
+        if let BVHNodeType::Internal{ ref mut right_offset, .. } = node_type {
+            *right_offset = roff;
         }
 
-        Some(nid)
+        nid
     }
 
     pub fn bounding_volume<'a>(&'a self) -> Option<&'a AABB> {
         if self.nodes.is_empty() {
             None
         } else {
-            Some(&self.nodes[0].bounds())
+            Some(&self.nodes[0].bounds)
         }
     }
 
     pub fn intersect(&self, ray: &Ray) -> Vec<T> {
         let mut results = Vec::new();
         if !self.nodes.is_empty() {
-            self.intersect_rec(ray, BVHNodeId(0), &mut results);
+            self.intersect_rec(ray, 0, &mut results);
         }
         results
     }
 
-    fn intersect_rec(&self, ray: &Ray, nid: BVHNodeId, results: &mut Vec<T>) {
-        match unsafe { self.nodes.get_unchecked(nid.0) } {
-            BVHNode::Internal{ left, right, ref bounds, .. } => {
-                if ray.intersects(bounds) {
-                    left.clone().map(|lid| self.intersect_rec(ray, lid, results));
-                    right.clone().map(|rid| self.intersect_rec(ray, rid, results));
-                }
-            },
+    fn intersect_rec(&self, ray: &Ray, offset: usize, results: &mut Vec<T>) {
+        let node = unsafe { self.nodes.get_unchecked(offset) };
+        if ray.intersects(&node.bounds) {
+            match node.node_type {
+                BVHNodeType::Internal{ right_offset, .. } => {
+                    self.intersect_rec(ray, offset+1, results);
+                    self.intersect_rec(ray, right_offset, results);
+                },
 
-            BVHNode::Leaf{ ref bounds, value } => {
-                if ray.intersects(bounds) {
-                    results.push(value.clone())
-                }
-            },
+                BVHNodeType::Leaf{ values_start, num_nodes } => {
+                    for value in self.values[values_start..].iter().take(num_nodes) {
+                        results.push(value.clone())
+                    }
+                },
+            }
         }
     }
 }
@@ -267,11 +271,11 @@ impl<T> BVH<T> where T: Clone {
 #[test]
 fn test_bvh() {
     {
-        let mut nodes = vec![
+        let nodes = vec![
             ( AABB::new([1.0, 1.0, 1.0].into(), [2.0, 2.0, 2.0].into()), 1 ),
         ];
 
-        let bvh = BVH::from_nodes(&mut nodes, &|(a,_)| a);
+        let bvh = BVH::from_nodes(nodes, &|(a,_)| a);
         assert_eq!(
             bvh.bounding_volume(),
             Some(&AABB::new([1.0, 1.0, 1.0].into(), [2.0, 2.0, 2.0].into()))
@@ -285,13 +289,13 @@ fn test_bvh() {
     }
 
     {
-        let mut nodes = vec![
+        let nodes = vec![
             ( AABB::new([1.0, 1.0, 1.0].into(), [2.0, 2.0, 2.0].into()), 1 ),
             ( AABB::new([3.0, 3.0, 1.0].into(), [4.0, 4.0, 2.0].into()), 2 ),
             ( AABB::new([0.5, 0.5, 1.0].into(), [2.0, 2.0, 2.0].into()), 3 ),
         ];
 
-        let bvh = BVH::from_nodes(&mut nodes, &|(a,_)| a);
+        let bvh = BVH::from_nodes(nodes, &|(a,_)| a);
         assert_eq!(
             bvh.bounding_volume(),
             Some(&AABB::new([0.5, 0.5, 1.0].into(), [4.0, 4.0, 2.0].into()))
