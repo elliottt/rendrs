1491252d0ecd0460115f37035fb091b11684468d
Trevor Elliott <awesomelyawesome@gmail.com>
2022-09-26 12:33:02 -0700

    Restructure the args

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use anyhow::Error;
-use clap::Parser;
+use clap::{Parser, Subcommand};
 
 mod bvh;
 mod camera;
@@ -16,43 +16,67 @@ use parser::Target;
 #[derive(Parser, Debug)]
 #[clap(author = "Trevor Elliott", version = "0.2")]
 struct Options {
-    #[clap(short,
+    #[clap(subcommand)]
+    command: Command,
+}
+
+#[derive(Subcommand, Debug)]
+enum Command {
+    Serve,
+
+    Render {
+        #[clap(short,
            long,
            help = "The number of threads to spawn",
            default_value_t = num_cpus::get() as u64,
            value_parser = clap::value_parser!(u64).range(1..=num_cpus::get() as u64),
-      )]
-    threads: u64,
+        )]
+        threads: u64,
 
-    #[clap(help = "The input scene file")]
-    scene: String,
+        #[clap(help = "The scene file to render")]
+        scene: String,
+    },
 }
 
 fn main() -> Result<(), Error> {
     let opts = Options::parse();
 
-    let input = std::fs::read_to_string(opts.scene)?;
-    let (scene, renders) = parser::parse(&input)?;
+    match opts.command {
+        Command::Serve => {
+            println!("serving!");
+        }
 
-    for render in renders {
-        let canvas = integrator::render(
-            render.canvas_info.clone(),
-            &scene,
-            render.root,
-            &render.integrator,
-            opts.threads as usize,
-        );
-        let width = canvas.width();
-        let height = canvas.height();
+        Command::Render { threads, scene } => {
+            let input = std::fs::read_to_string(scene)?;
+            let (scene, renders) = parser::parse(&input)?;
 
-        match render.target {
-            Target::File { path } => {
-                println!("Writing {}", &path.to_str().unwrap());
-                image::save_buffer(path, &canvas.data(), width, height, image::ColorType::Rgb8)
-                    .unwrap();
+            for render in renders {
+                let canvas = integrator::render(
+                    render.canvas_info.clone(),
+                    &scene,
+                    render.root,
+                    &render.integrator,
+                    threads as usize,
+                );
+                let width = canvas.width();
+                let height = canvas.height();
+
+                match render.target {
+                    Target::File { path } => {
+                        println!("Writing {}", &path.to_str().unwrap());
+                        image::save_buffer(
+                            path,
+                            &canvas.data(),
+                            width,
+                            height,
+                            image::ColorType::Rgb8,
+                        )
+                        .unwrap();
+                    }
+
+                    Target::Ascii => println!("{}", canvas.to_ascii()),
+                }
             }
-
-            Target::Ascii => println!("{}", canvas.to_ascii()),
         }
     }
 
