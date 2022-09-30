eb6def00832a5ff93b89072c91d9002e6ac4322f
Trevor Elliott <awesomelyawesome@gmail.com>
2022-09-30 00:23:32 -0700

    Fix a BVH construction bug by making min and max explicit

--- src/bvh.rs
+++ src/bvh.rs
@@ -3,9 +3,10 @@ use nalgebra::{Matrix4, Point3, Vector3};
 use crate::{ray::Ray, transform::ApplyTransform};
 
 #[derive(Debug, Clone, PartialEq)]
-pub struct BoundingBox {
-    min: Point3<f32>,
-    max: Point3<f32>,
+pub enum BoundingBox {
+    Min,
+    Max,
+    Bounds { min: Point3<f32>, max: Point3<f32> },
 }
 
 fn min_point(a: &Point3<f32>, b: &Point3<f32>) -> Point3<f32> {
@@ -20,82 +21,122 @@ impl BoundingBox {
     pub fn new(a: Point3<f32>, b: Point3<f32>) -> Self {
         let min = min_point(&a, &b);
         let max = max_point(&a, &b);
-        BoundingBox { min, max }
+        BoundingBox::Bounds { min, max }
     }
 
     pub fn centroid(&self) -> Point3<f32> {
-        self.min + self.extent()
+        match self {
+            Self::Min => Point3::origin(),
+            Self::Max => Point3::origin(),
+            Self::Bounds { min, .. } => min + self.extent(),
+        }
     }
 
+    #[inline]
     pub fn extent(&self) -> Vector3<f32> {
-        (self.max - self.min) / 2.
+        match self {
+            Self::Min => Vector3::new(0., 0., 0.),
+            Self::Max => Vector3::new(std::f32::INFINITY, std::f32::INFINITY, std::f32::INFINITY),
+            Self::Bounds { min, max } => (max - min) / 2.,
+        }
     }
 
     /// True when the bound contains no volume.
     pub fn is_empty(&self) -> bool {
-        self.min.x >= self.max.x && self.min.y >= self.max.y && self.min.z >= self.max.z
+        match self {
+            Self::Min => true,
+            Self::Max => false,
+            Self::Bounds { min, max } => min.x >= max.x && min.y >= max.y && min.z >= max.z,
+        }
     }
 
     pub fn min() -> Self {
-        BoundingBox {
-            min: Point3::new(std::f32::MAX, std::f32::MAX, std::f32::MAX),
-            max: Point3::new(std::f32::MIN, std::f32::MIN, std::f32::MIN),
-        }
+        Self::Min
     }
 
     pub fn max() -> Self {
-        BoundingBox {
-            min: Point3::new(std::f32::MIN, std::f32::MIN, std::f32::MIN),
-            max: Point3::new(std::f32::MAX, std::f32::MAX, std::f32::MAX),
-        }
+        Self::Max
     }
 
     pub fn union(&self, other: &Self) -> Self {
-        BoundingBox {
-            min: min_point(&self.min, &other.min),
-            max: max_point(&self.max, &other.max),
+        match (self, other) {
+            (Self::Min, _) => other.clone(),
+            (Self::Max, _) => Self::Max,
+            (_, Self::Min) => self.clone(),
+            (_, Self::Max) => Self::Max,
+            (Self::Bounds { min: lm, max: lx }, Self::Bounds { min: rm, max: rx }) => {
+                Self::Bounds {
+                    min: min_point(lm, rm),
+                    max: max_point(lx, rx),
+                }
+            }
         }
     }
 
     pub fn union_point(&self, other: &Point3<f32>) -> Self {
-        BoundingBox {
-            min: min_point(&self.min, other),
-            max: max_point(&self.max, other),
+        match self {
+            Self::Min => Self::Bounds {
+                min: other.clone(),
+                max: other.clone(),
+            },
+            Self::Max => Self::Max,
+            Self::Bounds { min, max } => Self::Bounds {
+                min: min_point(min, other),
+                max: max_point(max, other),
+            },
         }
     }
 
     pub fn intersect(&self, other: &Self) -> Self {
-        BoundingBox {
-            min: max_point(&self.min, &other.min),
-            max: min_point(&self.max, &other.max),
+        match (self, other) {
+            (Self::Min, _) => Self::Min,
+            (Self::Max, _) => other.clone(),
+            (_, Self::Min) => Self::Min,
+            (_, Self::Max) => self.clone(),
+            (Self::Bounds { min: lm, max: lx }, Self::Bounds { min: rm, max: rx }) => {
+                Self::Bounds {
+                    min: max_point(lm, rm),
+                    max: min_point(lx, rx),
+                }
+            }
         }
     }
 
     #[cfg(test)]
     pub fn contains(&self, p: &Point3<f32>) -> bool {
-        self.min < p.clone() && self.max > p.clone()
+        match self {
+            Self::Min => false,
+            Self::Max => true,
+            Self::Bounds { min, max } => min <= p && max >= p,
+        }
     }
 
     /// True when the ray would intersect this bounding box.
     pub fn intersects(&self, ray: &Ray) -> bool {
-        let t1 = Point3::new(
-            (self.min.x - ray.position.x) * ray.inv_direction.x,
-            (self.min.y - ray.position.y) * ray.inv_direction.y,
-            (self.min.z - ray.position.z) * ray.inv_direction.z,
-        );
-        let t2 = Point3::new(
-            (self.max.x - ray.position.x) * ray.inv_direction.x,
-            (self.max.y - ray.position.y) * ray.inv_direction.y,
-            (self.max.z - ray.position.z) * ray.inv_direction.z,
-        );
+        match self {
+            Self::Min => false,
+            Self::Max => true,
+            Self::Bounds { min, max } => {
+                let t1 = Point3::new(
+                    (min.x - ray.position.x) * ray.inv_direction.x,
+                    (min.y - ray.position.y) * ray.inv_direction.y,
+                    (min.z - ray.position.z) * ray.inv_direction.z,
+                );
+                let t2 = Point3::new(
+                    (max.x - ray.position.x) * ray.inv_direction.x,
+                    (max.y - ray.position.y) * ray.inv_direction.y,
+                    (max.z - ray.position.z) * ray.inv_direction.z,
+                );
 
-        let min = Point3::new(t1.x.min(t2.x), t1.y.min(t2.y), t1.z.min(t2.z));
-        let max = Point3::new(t1.x.max(t2.x), t1.y.max(t2.y), t1.z.max(t2.z));
+                let min = Point3::new(t1.x.min(t2.x), t1.y.min(t2.y), t1.z.min(t2.z));
+                let max = Point3::new(t1.x.max(t2.x), t1.y.max(t2.y), t1.z.max(t2.z));
 
-        let tmin = min.x.max(min.y).max(min.z);
-        let tmax = max.x.min(max.y).min(max.z);
+                let tmin = min.x.max(min.y).max(min.z);
+                let tmax = max.x.min(max.y).min(max.z);
 
-        tmax >= tmin
+                tmax >= tmin
+            }
+        }
     }
 }
 
@@ -116,8 +157,9 @@ fn test_union() {
 fn test_union_point() {
     let p = Point3::new(1., 1., 1.);
     let a = BoundingBox::min().union_point(&p);
-    assert_eq!(p, a.min);
-    assert_eq!(p, a.max);
+    println!("{:?}", p);
+    println!("{:?}", a);
+    assert!(a.contains(&p));
 }
 
 #[test]
@@ -129,19 +171,25 @@ fn test_intersect() {
 
 impl ApplyTransform for BoundingBox {
     fn transform(&self, m: &Matrix4<f32>) -> Self {
-        // compute the centroid and extent
-        let extent = self.extent();
-        let centroid = self.min + extent;
+        match self {
+            Self::Min => Self::Min,
+            Self::Max => Self::Max,
+            Self::Bounds {..} => {
+                // compute the centroid and extent
+                let extent = self.extent();
+                let centroid = self.centroid();
 
-        // transform the centroid to find the new origin
-        let centroid = centroid.transform(m);
+                // transform the centroid to find the new origin
+                let centroid = centroid.transform(m);
 
-        // transform the extent by the abs matrix to find the new extent
-        let extent = extent.transform(&m.abs());
+                // transform the extent by the abs matrix to find the new extent
+                let extent = extent.transform(&m.abs());
 
-        Self {
-            min: centroid - extent,
-            max: centroid + extent,
+                Self::Bounds {
+                    min: centroid - extent,
+                    max: centroid + extent,
+                }
+            }
         }
     }
 }
@@ -156,6 +204,11 @@ fn test_bounding_box_transform() {
     let total = bound.union(&other);
 
     assert!(total.contains(&Point3::new(0.5, 0.5, 0.5)));
+
+    let bound = BoundingBox::max();
+    let other =
+        bound.apply(&Transform::new().rotate(&Vector3::new(std::f32::consts::FRAC_PI_2, 0., 0.)));
+    assert_eq!(bound, other);
 }
 
 #[derive(Debug, Clone)]
@@ -290,16 +343,22 @@ impl<T: Clone> BVH<T> {
 }
 
 fn largest_axis(bound: &BoundingBox) -> (f32, Axis) {
-    let diff = bound.max - bound.min;
-    if diff.x > diff.y {
-        if diff.x > diff.z {
-            (bound.min.x + diff.x / 2., Axis::X)
-        } else {
-            (bound.min.z + diff.z / 2., Axis::Z)
+    match bound {
+        BoundingBox::Min => (0., Axis::X),
+        BoundingBox::Max => (std::f32::INFINITY, Axis::X),
+        BoundingBox::Bounds {min, max} => {
+            let diff = max - min;
+            if diff.x > diff.y {
+                if diff.x > diff.z {
+                    (min.x + diff.x / 2., Axis::X)
+                } else {
+                    (min.z + diff.z / 2., Axis::Z)
+                }
+            } else if diff.x > diff.z {
+                (min.x + diff.x / 2., Axis::X)
+            } else {
+                (min.y + diff.y / 2., Axis::Y)
+            }
         }
-    } else if diff.x > diff.z {
-        (bound.min.x + diff.x / 2., Axis::X)
-    } else {
-        (bound.min.y + diff.y / 2., Axis::Y)
     }
 }
