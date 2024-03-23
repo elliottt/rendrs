bd7067344cf37291ef783459ff33dfbada8bf695
Trevor Elliott <awesomelyawesome@gmail.com>
2024-03-23 12:53:41 -0700

    Add a nix flake

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  description = "Development environment for elliottt/story";
+
+  inputs = {
+    nixpkgs.url      = "github:NixOS/nixpkgs/nixos-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    flake-utils.url  = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, rust-overlay, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+
+        rust = pkgs.rust-bin.stable.latest.default;
+
+      in
+      with pkgs;
+      {
+        devShells.default = mkShell {
+          buildInputs = [
+            rust
+            pkgs.rust-analyzer
+          ];
+        };
+      }
+    );
+}
